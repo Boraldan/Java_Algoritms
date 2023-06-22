ca851cebc7007122213c73ee49bd98dbdd02433a
Boraldan <boraldan@gmail.com>
2023-06-22 18:16:40 +0300

    dz_4

---
+++ DZ4/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+    public static void main(String[] args) {
+        RedBlackTree rbtree = new RedBlackTree ();
+        rbtree.add(1);
+        rbtree.add(10);
+        rbtree.add(15);
+        rbtree.add(11);
+        System.out.println(rbtree.add(17));
+
+
+
+
+
+    }
+}
